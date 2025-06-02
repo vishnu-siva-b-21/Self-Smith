a3ba30436792d8967c0f66cb2d2fd2172c89c690
vishnu-siva-b-21 <vishnu.siva.b.21@gmail.com>
2025-06-02 10:10:02 +0530

    Update config.py

--- self_smith_app/config.py
+++ self_smith_app/config.py
@@ -15,9 +15,9 @@ class Config(object):
     }
 
 
-MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-MAIL_USERNAME = os.getenv("MAIL_USERNAME", None)
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", None)
-MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
-MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
-MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "your_email@gmail.com")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "your_password_or_app_password")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
